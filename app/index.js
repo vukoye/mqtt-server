6b968cbcbbdda4a4fcf5ae2ef76d3bda3efbcefe
nemanja <nemanjavu@gmail.com>
2017-11-01 15:51:07 +0100

    Adding winston logging

--- app/index.js
+++ app/index.js
@@ -3,6 +3,31 @@ var mosca = require('mosca')
 var SECURE_KEY = __dirname + '/newkey.pem';
 var SECURE_CERT = __dirname + '/cert.pem';
 
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    //
+    // - Write to all logs with level `info` and below to `combined.log` 
+    // - Write all logs error (and below) to `error.log`.
+    //
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' })
+  ]
+});
+
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+// 
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple()
+  }));
+}
+
+
+
 var settings = {
     port: 1883,
     logger: {
@@ -19,15 +44,18 @@ var server = new mosca.Server(settings);
 server.on('ready', setup);
 
 server.on('clientConnected', function(client) {
+    logger.info('Hello again distributed logs');
     console.log('client connected', client.id);
 });
 
 // fired when a message is received
 server.on('published', function(packet, client) {
+    logger.info('Hello again distributed logs');
     console.log('Published', packet.payload);
 });
 
 // fired when the mqtt broker is ready
 function setup() {
+    logger.info('Hello again distributed logs');
     console.log('Mosca embedded MQTT broker running now')
 }
