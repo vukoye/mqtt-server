2b88975d28445ce8f700e4e067ef242acb369733
vukoye <nemanjavu@gmail.com>
2017-11-02 08:19:27 +0100

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,29 +1,6 @@
 'use strict';
 var mosca = require('mosca')
 
-
-// We’re setting up an extremely simple server here.
-const http = require('http');
-
-// These could (should) be set as env vars.
-const port = process.env.PORT || 5000;
-const host = process.env.HOST || 'localhost';
-
-// No matter what hits the server, we send the same thing.
-http.createServer((req, res) => {
-
-  // Tell the browser what’s coming.
-  res.writeHead(200, {
-    'Content-Type': 'text/html; charset=utf-8',
-  });
-
-  // Send a simple message in HTML.
-  res.write('<h1>I’m a Node app!</h1>');
-  res.write('<p>And I’m <em>sooooo</em> secure.</p>');
-  res.end();
-}).listen(port, host);
-
-
 var SECURE_KEY = __dirname + '/newkey.pem';
 var SECURE_CERT = __dirname + '/cert.pem';
 
